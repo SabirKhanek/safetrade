d5d9298f11e09bf3219028615e09bf1e175a774f
Sabir Khan <sabirkhanek66@gmail.com>
2024-04-27 21:37:20 +0500

    feat: added clustering with pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,15 +4,19 @@ module.exports = {
       name: "Sourtech Server",
       script: "dist/main.js",
       cwd: "./apps/server",
+      instances: "max",
+      exec_mode: "cluster",
     },
     {
       name: "Sourtech Client",
       script: "../../node_modules/next/dist/bin/next",
       args: "start",
       cwd: "./apps/client",
+      instances: "max",
+      exec_mode: "cluster",
       env: {
-        PORT: 3001
-      }
+        PORT: 3001,
+      },
     },
   ],
 };
