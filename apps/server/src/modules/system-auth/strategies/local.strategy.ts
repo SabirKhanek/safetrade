315ccfe651d701f1c25f45bf573a59a3b6160c1f
Sabir Khan <sabirkhanek66@gmail.com>
2024-06-26 03:25:24 +0500

    fix

--- apps/server/src/modules/system-auth/strategies/local.strategy.ts
+++ apps/server/src/modules/system-auth/strategies/local.strategy.ts
@@ -17,7 +17,7 @@ export class LocalStrategy extends PassportStrategy(Strategy, 'system-local') {
   async validate(req, username: string, password: string): Promise<any> {
     const ip = configuration().development
       ? '182.181.136.138'
-      : req.headers['x-real-ip'] || req.headers['x-forwarded-for'];
+      : req.headers['x-forwarded-for']?.split(',').at(0);
     const userAgent = req.headers['user-agent'];
     const user = await this.authService.authenticate(username, password, {
       ip_address: ip,
